e4f42dd537153d81560ee8bcc81217e4f6b2d77c
franklinmelo <franklinmelo18@gmail.com>
2017-11-24 11:06:15 -0300

    tela de login

--- src/controller/TelaLoginController.java
+++ src/controller/TelaLoginController.java
@@ -1,21 +1,47 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import fwboarding.ClassePrincipal;
-import javafx.application.Platform;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+public class TelaLoginController implements Initializable {
 
-public class TelaLoginController implements Initializable{
+	public class PleaseProvideControllerClassName {
 
+		@FXML
+		private TextField textUsuarioLogin;
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		@FXML
+		private PasswordField textSenhaLogin;
+
+		@FXML
+		private Button buttonCancelar;
+
+		@FXML
+		private Button buttonConectar;
+
+		@FXML
+		void clickOnCancelar(ActionEvent event) {
+
+		}
+
+		@FXML
+		void clickOnConectar(ActionEvent event) {
+
+		}
 
 	}
 
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
