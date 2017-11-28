aff698a51cc904083291a8263b31ebb644703026
Joab Leite <leitejoab@gmail.com>
2017-11-28 16:30:48 -0300

    added TelaLogin

--- src/controller/TelaLoginController.java
+++ src/controller/TelaLoginController.java
@@ -6,42 +6,41 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
 public class TelaLoginController implements Initializable {
 
-	public class PleaseProvideControllerClassName {
+	@FXML
+	private TextField textUsuarioLogin;
 
-		@FXML
-		private TextField textUsuarioLogin;
+	@FXML
+	private PasswordField textSenhaLogin;
 
-		@FXML
-		private PasswordField textSenhaLogin;
+	@FXML
+	private Button buttonConectar;
 
-		@FXML
-		private Button buttonCancelar;
 
-		@FXML
-		private Button buttonConectar;
 
-		@FXML
-		void clickOnCancelar(ActionEvent event) {
+	private Stage dialogStage;
 
-		}
-
-		@FXML
-		void clickOnConectar(ActionEvent event) {
-
-		}
-
-	}
-
-	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	public void setDialogStage(Stage dialogStage) {
+		this.dialogStage = dialogStage;
+
+	}
+
+	@FXML
+	void clickOnConectar() {
+		dialogStage.close();
 	}
 
 }
--- src/fwboarding/ClassePrincipal.java
+++ src/fwboarding/ClassePrincipal.java
@@ -2,6 +2,7 @@ package fwboarding;
 
 import java.io.IOException;
 
+import controller.TelaLoginController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -15,11 +16,12 @@ public class ClassePrincipal extends Application {
 
 	public static Stage stage;
 	public static BorderPane rootLayout;
+	
 
 	@Override
 	public void start(Stage stage) throws IOException {
 		ClassePrincipal.stage = stage;
-
+		carregarTelaLogin();
 		initRootLayout();
 
 	}
@@ -38,8 +40,9 @@ public class ClassePrincipal extends Application {
 			stage.setTitle("FwBoarding");
 			stage.setMaximized(false);
 			stage.getIcons().add(new Image(this.getClass().getResource("/view/images/Icons/IconNavio.png").toString()));
-			stage.show();
 
+			stage.show();
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -58,6 +61,8 @@ public class ClassePrincipal extends Application {
 	        dialogStage.initModality(Modality.WINDOW_MODAL);
 	        dialogStage.initOwner(stage);
 	        dialogStage.setResizable(false);
+	        
+	        
 	        Scene scene = new Scene(page);
 	        dialogStage.setScene(scene);
 
@@ -78,6 +83,9 @@ public class ClassePrincipal extends Application {
 		dialogStage.initOwner(stage);
 		dialogStage.setResizable(false);
 		
+		TelaLoginController controller = loader.getController();
+        controller.setDialogStage(dialogStage);
+        
 		Scene scene = new Scene(page);
 		dialogStage.setScene(scene);
 		dialogStage.showAndWait();
