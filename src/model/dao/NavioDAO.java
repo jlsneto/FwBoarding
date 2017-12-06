7cab31a8917d5e032b3d5cf4c7bfe960d305545b
Joab Leite <leitejoab@gmail.com>
2017-12-06 00:18:40 -0300

    fixed bug id CadastroNavio

--- src/model/dao/NavioDAO.java
+++ src/model/dao/NavioDAO.java
@@ -6,16 +6,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import model.database.DatabaseFactory;
+import model.database.DatabaseParams;
 import model.vo.NavioVO;
 import model.vo.PaisVO;
 import view.ConstruirDialog;
 
 public class NavioDAO {
 
+	private Properties props = DatabaseParams.getProp();
+	private String base = props.getProperty("database");
 	private Connection conn;
 
 	public NavioDAO() {
@@ -88,11 +92,11 @@ public class NavioDAO {
 			PreparedStatement stmt = conn.prepareStatement(sql);
 			stmt.setLong(1, codigoNavio);
 			ResultSet result = stmt.executeQuery();
-			
-			if(result.next()) {
+
+			if (result.next()) {
 				throw new Exception();
 			}
-			
+
 		} catch (SQLException e) {
 			ConstruirDialog erro = new ConstruirDialog();
 			erro.DialogError("SQLException", "Erro ao consultar o banco de dados", e.getErrorCode(), e.getMessage(),
@@ -146,13 +150,20 @@ public class NavioDAO {
 	}
 
 	public int verificaUltimoCodigo() {
-		String sql = "SELECT LAST_VALUE FROM CADNAVIO_CODIGONAVIO_SEQ";
+
+		String sql;
+
+		if (this.base.equals("oracle")) {
+			sql = "SELECT (LAST_NUMBER-1) as LAST_NUMBER FROM USER_SEQUENCES WHERE SEQUENCE_NAME = 'SEQ_CODIGO_CADNAVIO'";
+		} else {
+			sql = "SELECT LAST_VALUE AS LAST_NUMBER FROM CADNAVIO_CODIGONAVIO_SEQ";
+		}
 
 		try {
 			PreparedStatement stmt = conn.prepareStatement(sql);
 			ResultSet resultado = stmt.executeQuery();
 			if (resultado.next()) {
-				return resultado.getInt("LAST_VALUE");
+				return resultado.getInt("LAST_NUMBER");
 			} else {
 				return 0;
 			}
